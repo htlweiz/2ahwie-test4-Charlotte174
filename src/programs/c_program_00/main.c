e96e3b542ed13f42883fe9e6a58f4553194e8081
Charlotte174 <chaleng@outlook.de>
2025-05-26 08:53:28 +0000

    exercise 2

--- src/programs/c_program_00/main.c
+++ src/programs/c_program_00/main.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
 
-int main(int argc, char **argv) {
+int int_even_odd(int zahl1)
+{
+  if (zahl1 % 2 == 0)
+  {
+    printf("%d... gerade Zahl wurde übergeben", zahl1);
+  }
+  else if (zahl1 % 2 != 0)
+  {
+    printf("%d...ungerade Zahl wurde übergeben", zahl1);
+  }
+  else if (zahl1 == 0)
+  {
+    printf("%d... die Zahl 0 wurde übergeben", zahl1);
+  }
+  return zahl1;
+}
+
+int main(int argc, char **argv)
+{
+//Exercise 1:
+
+
+//Exercise 2:
+int array2[10]= {3,1,7,4,9,1,5,10,-2,2};
+for (int i = 0; i <10 ; i++)
+{
+  printf("Zahl 1: %d \n",array2[i]);
+}
+
+
   printf("This program was called with %d arguments.\n", argc);
   printf("Hello World!\n");
 }
