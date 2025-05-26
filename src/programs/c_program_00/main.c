329571b7032c11fb85e8df22cb78ebbb3723050e
Charlotte174 <chaleng@outlook.de>
2025-05-26 09:22:13 +0000

    Erxercise 1 main

--- src/programs/c_program_00/main.c
+++ src/programs/c_program_00/main.c
@@ -17,39 +17,39 @@ int int_even_odd(int zahl1)
   return zahl1;
 }
 
-//Exercise 3:
+// Exercise 3:
 int int_numbers_odd_even(int int_numbers)
 {
-  if (int_numbers %2 == 0)
+  if (int_numbers % 2 == 0)
   {
     int even_numbers = summ int_numbers;
     printf("Anzahl der geraden Zahlen: %d", even_numbers);
     return even_numbers;
-  } else if (int_numbers %2 != 0)
+  }
+  else if (int_numbers % 2 != 0)
   {
     int odd_numbers = summ int_numbers;
     printf("Anzahl der ungeraden Zahlen: %d", odd_numbers);
     return odd_numbers;
   }
 
-int int_numbers_summ(int int_numbers)
-{
-  int summ = summ int_numbers;
-  printf(" Summe aller Zahlen : %d", summ)
-}  return summ
-
+  int int_numbers_summ(int int_numbers)
+  {
+    int summ = summ int_numbers;
+    printf(" Summe aller Zahlen : %d", summ)
+  }
+  return summ
 }
-
 
 int main(int argc, char **argv)
 {
   // Exercise 1:
-
+printf(int_even_odd);
   // Exercise 2:
   int array2[10] = {3, 1, 7, 4, 9, 1, 5, 10, -2, 2};
   for (int i = 0; i < 10; i++)
   {
-    printf("Zahl %d: %d \n", i+1, array2[i]);
+    printf("Zahl %d: %d \n", i + 1, array2[i]);
   }
 
   // Exercise 3:
@@ -62,7 +62,7 @@ int main(int argc, char **argv)
   for (int i = 0; i < 5; i++)
   {
     printf("%d\n", int_numbers[i]);
-  } 
+  }
 
   printf("This program was called with %d arguments.\n", argc);
   printf("Hello World!\n");
